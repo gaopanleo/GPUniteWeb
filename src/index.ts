1f3295ff80ceb975f8b4dfadab50d4e64b33aa37
gaopanleo <52186764+gaopanleo@users.noreply.github.com>
2025-05-30 11:09:56 +0800

    Update index.ts
    修复

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { renderHtml } from "./renderHtml";
 import { homeHtml } from "./home";
 export default {
   async fetch(request, env) {
-   /* const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");*/
-    const { results } = await stmt.all(); 
+  const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
+  const { results } = await stmt.all(); 
 
    /* return new Response(renderHtml(JSON.stringify(results, null, 2)), {
       headers: {
